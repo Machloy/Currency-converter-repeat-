e81c4dc6b027973ed9ee87f4434eb4e8924a7066
Maciej Godlewski <96136252+Machloy@users.noreply.github.com>
2022-10-19 18:58:19 +0200

    added new function calculateResult

--- script/script.js
+++ script/script.js
@@ -1,18 +1,26 @@
 {
-  const ammountElement = document.querySelector(".js-ammount");
+  const amountElement = document.querySelector(".js-ammount");
   const currencyElement = document.querySelector(".js-currency");
   const resultTxt = document.querySelector(".js-result");
   const formElement = document.querySelector(".js-form");
 
+  const calculateResult = (amount, exchangeRate) => {
+    return amount / exchangeRate;
+  };
+
   const onFormSubmit = (event) => {
     event.preventDefault();
 
-    const currency = currencyElement.value;
-    const ammount = ammountElement.value;
-    const result = ammount / currency;
+    const exchangeRate = currencyElement.value;
+    const amount = amountElement.value;
+    const result = calculateResult(amount, exchangeRate);
 
     resultTxt.innerText = `${result.toFixed(2)}`;
   };
 
-  formElement.addEventListener("submit", onFormSubmit);
+  const init = () => {
+    formElement.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
 }
